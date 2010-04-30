e524cdf6e93c39a14f1251090373560fa173f246
Masahiro Kimoto <ryutorion@msn.com>
2010-04-30 17:14:12 +0900

    Commit before release

--- chrome/content/autoscroller.js
+++ chrome/content/autoscroller.js
@@ -1,88 +1,159 @@
 if(!AutoScroller)
-  var AutoScroller = {};
+    var AutoScroller = {};
 
 (function(){
   window.addEventListener("load", init, false);
 
-  var state;
   var options;
+
+  var state = 0;
+  var interval;
   var menu;
-  var line = { interval : 0, delta : 0, num : 0 };
-  var smooth = { interval : 0, delta : 0, num : 0 };
-  var mode;
+  var move;
+  var delta;
+  var before;
   var orient = true;
-  var timer;
+  var w;
+  var id = null;
+  var id2content = [];
 
   function init(){
-    state = false;
     options = Components.classes["@mozilla.org/preferences-service;1"]
                 .getService(Components.interfaces.nsIPrefService)
                 .getBranch("extensions.autoscroller.");
 
-    menu = document.getElementById("autoscroller.execute");
+    interval = options.getIntPref("interval");
+    delta    = options.getIntPref("delta");
+    move     = options.getIntPref("move");
 
-    line.interval = options.getIntPref("line.interval") || 1000;
-    line.delta    = options.getIntPref("line.delta") || 100;
-    line.num      = options.getIntPref("line.number") || 1;
+    var keys = [
+        "trigger", "back", "accel", "brake", "orient"
+    ];
+    for(var i = 0; i < keys.length; ++i){
+        var k = keys[i];
 
-    mode = options.getIntPref("mode") || 1;
+        var key     = options.getCharPref("keys." + k + ".key");
+        var accel   = options.getBoolPref("keys." + k + ".accel");
+        var alt     = options.getBoolPref("keys." + k + ".alt");
 
-    smooth.intervale = options.getIntPref("smooth.interval");
-    smooth.deltae    = options.getIntPref("smooth.delta");
-    smooth.num       = options.getIntPref("smooth.number");
+        setShortcut(k, key, accel, alt);
+    }
 
     options.QueryInterface(Components.interfaces.nsIPrefBranch2);
     options.addObserver("", observer, false);
+
+    menu = document.getElementById("autoscroller.menu.trigger");
   }
 
-  AutoScroller.execute = function(){
-    if(state){
-      // When scrolling
-    }else{
-      // When not scrolling
-    }
+  function setShortcut(target, key, accel, alt){
+      var elm = document.getElementById("autoscroller.keys." + target);
+
+      if(!elm)
+          return ;
+
+      if(key != undefined)
+          elm.setAttribute("key", key);
+
+      if(accel != undefined && alt != undefined){
+          var modifiers = accel ? "accel" : "";
+          if(alt)
+              modifiers = accel ? modifiers + " alt" : "alt";
+
+          elm.setAttribute("modifiers", modifiers);
+      }
+  }
+
+  function scroll(){
+      if(orient)
+          w.scrollBy(0, move);
+      else
+          w.scrollBy(0, -move);
+
+      if(before == w.scrollY)
+          AutoScroller.trigger();
+      else
+          before = w.scrollY;
+  }
+
+  function start(target){
+      w = target || content;
+      height = w.document.documentElement.scrollHeight;
+      id = w.setInterval(scroll, 1000 / interval);
+  }
+
+  function stop(){
+      w.clearInterval(id);
+      id = null;
+  }
+
+  AutoScroller.trigger = function(){
+      if(id == null){
+          menu.setAttribute("checked", "true");
+          start(content);
+      }else{
+          menu.setAttribute("checked", "false");
+          stop();
+      }
   };
 
-  AutoScroller.reverse = function(){
+  AutoScroller.back = function(){
+      orient = false;
+      if(id == null)
+          AutoScroller.trigger();
   };
 
-  AutoScroller.accelerate = function(){
+  AutoScroller.accel = function(){
+      move += delta;
+      options.setIntPref("move", move);
   };
 
   AutoScroller.brake = function(){
+      move = move - delta < 0 ? 0 : move - delta;
+      options.setIntPref("move", move);
   };
 
-  function lineScroll(){
-  }
+  AutoScroller.orient = function(){
+      orient = !orient;
+      var elm = document.getElementById("autoscroller.keys.orient");
+      elm.setAttribute("checked", orient);
+  };
 
-  function smoothScroll(){
+  function parseShortcut(data){
+      var result = data.match(/autoscroller\.keys\.([^.]+?)\.[^.]+$/);
+      if(result != null){
+          var base = "autoscroller.keys." + result[1];
+          var key   = options.getCharPref(base + ".key");
+          var accel = options.getBoolPref(base + ".accel");
+          var alt   = options.getBoolPref(base + ".alt");
+
+          setShortcut(result[1], key, accel, alt);
+      }
   }
 
   var observer = {
     unregister : function(){
-      if(options)
-        options.removeObserver("", this);
+        if(options)
+            options.removeObserver("", this);
     },
     observe : function(subject, topic, data){
-      if(topic != "nsPref:changed")
-        return ;
+        if(topic != "nsPref:changed")
+            return ;
 
-      switch(data){
-      case "mode":
-        break;
-      case "line.interval":
-        break;
-      case "line.delta":
-        break;
-      case "line.number":
-        break;
-      case "smooth.interval":
-        break;
-      case "smooth.delta":
-        break;
-      case "smooth.number":
-        break;
-      }
+        switch(data){
+        case "interval":
+            interval = options.getIntPref("interval");
+            stop();
+            start(w);
+            break;
+        case "move":
+            move = options.getIntPref("move");
+            break;
+        case "delta":
+            delta = options.getIntPref("delta");
+            break;
+        default:
+            parseShortcut(data);
+        }
     }
   };
 })();
